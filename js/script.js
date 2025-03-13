35282ba46e4969f2f98c6795cbd7fb128db5d202
Shubham Sourabh <39551118+vampirepapi@users.noreply.github.com>
2025-03-13 14:53:01 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function toggleNode(el) {
-      // Only toggle if the node has children
-      if (el.querySelector('.children')) {
-        el.classList.toggle('expanded');
-        event.stopPropagation(); // Prevent parent nodes from toggling
-      }
-    }
+  // Only toggle if the node has children
+  if (el.querySelector('.children')) {
+    el.classList.toggle('expanded');
+    event.stopPropagation(); // Prevent parent nodes from toggling
+  }
+}
